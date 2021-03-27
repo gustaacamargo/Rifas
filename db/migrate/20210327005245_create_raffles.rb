71d5872d3ec1937bd8bc498506a9fa8f0acd9828
Gustavo <camargo25.gustavo@gmail.com>
2021-03-26 22:36:36 -0300

    Modifica atributos nas migrations para suportar valores nulos

--- db/migrate/20210327005245_create_raffles.rb
+++ db/migrate/20210327005245_create_raffles.rb
@@ -4,11 +4,11 @@ class CreateRaffles < ActiveRecord::Migration[6.1]
       t.references :user, null: false, foreign_key: true
       t.references :kind, null: false, foreign_key: true
       t.string :title
-      t.text :description
+      t.text :description, null: true
       t.datetime :probable_raffle_date
       t.datetime :start_date_sale
       t.datetime :end_date_sale
-      t.datetime :date_raffle
+      t.datetime :date_raffle, null: true
       t.float :ticket_value
 
       t.timestamps
