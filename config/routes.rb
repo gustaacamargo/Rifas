a30975880a8a1e645f37e8d2fdbfeda25d184916
Gustavo <camargo25.gustavo@gmail.com>
2021-03-27 23:47:45 -0300

    Adiciona autenticação

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :tickets
   resources :raffles
   resources :kinds
-  root to: 'raffles#index'
+  devise_scope :user do
+    root to: 'raffles#index'
+    get 'users/sign_out' => "devise/sessions#destroy"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
