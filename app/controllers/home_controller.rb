db18a834b011bc0b787b028f73083850e79ffedb
Gustavo <camargo25.gustavo@gmail.com>
2021-04-12 22:35:24 -0300

    Adiciona mais informações a tela inicial do usuário; Corrige exibição de datas na lista de rifas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @tickets = current_user
-                .tickets
-                .includes(:raffle, :award)
+    @raffled_not_tickets = current_user.tickets.includes(:award).joins(:raffle).where('probable_raffle_date > ?', DateTime.now).group('tickets.id')
+    @raffled_tickets = current_user.tickets.includes(:award).joins(:raffle).where('probable_raffle_date < ?', DateTime.now).group('tickets.id')
+
+    @total_ticket_value = current_user.tickets.joins(:raffle).sum(:ticket_value)
+    @purchased_tickets = current_user.tickets.size
   end
 end
